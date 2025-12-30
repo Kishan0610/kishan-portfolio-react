fbb4286d13f63400e8c40ac4ceee26c9cca1e8cc
Ravikishan Gupta <81710876+Kishan0610@users.noreply.github.com>
2025-12-30 12:51:49 +0530

    Update CV download link in About page

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -218,7 +218,7 @@ const AboutPage = () => {
             </p>
 
             <div className="flex flex-col lg:flex-row items-center lg:items-start gap-4 lg:gap-4 lg:px-0 w-full">
-              <a href="https://drive.google.com/file/d/1lnOkGhsulwWRuFzej3Bu6uzVkRCqJg2-/view?usp=drivesdk" className="w-full lg:w-auto">
+              <a href="https://drive.google.com/file/d/1oQbHIJ1rbPFMm9wgc7Km7AJofyi9lMxu/view?usp=drive_link" className="w-full lg:w-auto">
               <button 
                 aria-label="Download CV"
                 data-aos="fade-up"
